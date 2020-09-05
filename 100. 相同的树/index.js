a79bf211d63fe56e6a07443ca54ef51bfa12aa9a
tancgo <542112893@qq.com>
2020-09-05 21:10:49 +0800

    相同的树(分治思想)

---
+++ 100. 相同的树/index.js
@@ -0,0 +1,17 @@
+//分治递归
+var isSameTree = function (p, q) {
+  let isSame = false;
+  if (p === null && q === null) return true;
+
+  if (
+    p &&
+    q &&
+    p.val === q.val &&
+    isSameTree(p.left, q.left) &&
+    isSameTree(p.right, q.right)
+  ) {
+    return true;
+  }
+
+  return false;
+};
