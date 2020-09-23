daea3cc61aac043eda34101070fee02abcff4b59
tancgo <542112893@qq.com>
2020-09-23 09:02:59 +0800

    最长公共前缀

--- 014. 最长公共前缀/index.js
+++ 014. 最长公共前缀/index.js
@@ -1,16 +1,20 @@
+// 理解题意 公共前缀意思是从第一个字符开始
 var longestCommonPrefix = function (strs) {
-  if (strs.length == 0)
-    return "";
-  let ans = strs[0];
+  if (!strs.length) return "";
+  let target = strs[0]; // 取第一个字符串与第二个对比，用这俩的公共前缀继续比对下去
+
   for (let i = 1; i < strs.length; i++) {
-    let j = 0;
-    for (; j < ans.length && j < strs[i].length; j++) {
-      if (ans[j] != strs[i][j])
+    // console.log(strs[i])
+    for (var j = 0; j < target.length && j < strs[i].length; j++) {
+      // console.log(target[j], strs[i][j])
+      if (target[j] !== strs[i][j]) {
         break;
+      }
     }
-    ans = ans.substr(0, j);
-    if (ans === "")
-      return ans;
+
+    target = target.substr(0, j);
+    // 这个加上是优化， 如果公共前缀为空的话可以提前终止循环
+    if (target === "") return target;
   }
-  return ans;
+  return target;
 };
