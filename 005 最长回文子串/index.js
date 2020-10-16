6dd5456888ae6069e90187608ee23dde98bb18ba
tancgo <542112893@qq.com>
2020-10-16 08:45:14 +0800

    最长回文子串(动态规划)

--- 005 最长回文子串/index.js
+++ 005 最长回文子串/index.js
@@ -81,3 +81,50 @@ var longestPalindrome = function (s) {
 
   return s.substring(start, start + maxLen);
 };
+
+// 解法二中的表实际上只用了一半 可以优化空间
+/**
+ * 
+ [
+  [ true ],
+  [ false, true ],
+  [ true, false, true ],
+  [ false, true, false, true ],
+  [ false, false, false, false, true ]
+]
+ */
+var longestPalindrome = function (s) {
+  const len = s.length;
+  if (len < 2) return s;
+
+  let maxLen = 1;
+  let start = 0;
+
+  const dp = Array.from(Array(len), (_, index) => Array(index + 1));
+
+  for (let i = 0; i < len; i++) {
+    dp[i][i] = true;
+  }
+
+  for (let i = 1; i < len; i++) {
+    for (let j = 0; j < i; j++) {
+      if (s[i] !== s[j]) {
+        dp[i][j] = false;
+      } else {
+        // cbbd这种情况需要判断一下 不然dp[i][j] = dp[i - 1][j + 1]=dp[1][2] = null
+        if (i - j < 3) {
+          dp[i][j] = true;
+        } else {
+          dp[i][j] = dp[i - 1][j + 1];
+        }
+      }
+
+      if (dp[i][j] && i - j + 1 > maxLen) {
+        maxLen = i - j + 1;
+        start = j;
+      }
+    }
+  }
+
+  return s.substring(start, start + maxLen);
+};
