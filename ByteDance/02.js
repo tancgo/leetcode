e9b05557697e82958e5a858e1a44236a5ae8ed0a
tancgo <542112893@qq.com>
2020-10-26 19:54:00 +0800

    最少硬币组合(回溯)

--- ByteDance/02.js
+++ ByteDance/02.js
@@ -7,13 +7,51 @@
 
 const coins = [1, 5, 10, 25];
 
-class MiniCoinChange {
-  constructor(conis) {
-    this.conis = conis;
-  }
-  makeChange(n) {
-  }
-}
+// class MiniCoinChange {
+//   constructor(conis) {
+//     this.conis = conis;
+//   }
+//   makeChange(n) {
+//   }
+// }
 
-const miniCoinChange = new MiniCoinChange(coins);
-console.log(miniCoinChange.makeChange(36));
+// const miniCoinChange = new MiniCoinChange(coins);
+// console.log(miniCoinChange.makeChange(36));
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (candidates, target) {
+  const res = {};
+  let min = Infinity;
+
+  const dfs = (target, temp, id) => {
+    if (id === candidates.length) return;
+
+    // console.log(temp, 'ans')
+    if (target === 0) {
+      // console.log(temp)
+      res[temp.length] = temp;
+      min = Math.min(min, temp.length);
+      return;
+    }
+
+    // console.log(target, temp, candidates[id + 1], 'a')
+    dfs(target, temp, id + 1);
+
+    target -= candidates[id];
+
+    if (target >= 0) {
+      // console.log(target, [...temp, candidates[id]], candidates[id], 'b')
+      dfs(target, [...temp, candidates[id]], id);
+    }
+  };
+
+  dfs(target, [], 0);
+
+  return res[min]
+};
+
+console.log(combinationSum([1, 5, 10, 25], 36));
