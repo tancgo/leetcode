5d4c52ddef74d8d6e232e74dc2902d20909d0db9
tancgo <542112893@qq.com>
2020-10-28 10:39:31 +0800

    全排列(回溯)二次练习

--- 046. 全排列/index.js
+++ 046. 全排列/index.js
@@ -16,3 +16,28 @@ var permute = function (nums) {
   backtrack([]);
   return result;
 };
+
+// 练习回溯算法时  第二次做该题
+var permute = function (nums) {
+  const res = [];
+
+  function dfs(temp) {
+    // console.log(temp)
+    if (temp.length === nums.length) {
+      res.push(temp.slice());
+
+      return;
+    }
+
+    for (const n of nums) {
+      if (temp.includes(n)) continue;
+      temp.push(n);
+      dfs(temp);
+      temp.pop();
+    }
+  }
+
+  dfs([]);
+
+  return res;
+};
