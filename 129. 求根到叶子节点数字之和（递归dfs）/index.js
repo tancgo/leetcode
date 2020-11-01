f1393a3f034c28ca46e27db8d2493b6737ea77bb
tancgo <542112893@qq.com>
2020-11-01 12:55:40 +0800

    求根到叶子节点数字之和(回溯 | 递归)

--- 129. 求根到叶子节点数字之和（递归dfs）/index.js
+++ 129. 求根到叶子节点数字之和（递归dfs）/index.js
@@ -19,20 +19,25 @@ var sumNumbers = function (root) {
 var sumNumbers = function (root) {
   const res = [];
   function dfs(node, cur) {
+
+    console.log(node, cur)
+
     if (node === null) return 0;
 
     cur.push(node.val);
+    
 
     if (node.left === null && node.right === null) {
       // console.log(cur)
+      
       res.push(cur.slice());
-      cur.pop();
+      // cur.pop();
 
-      return cur;
+      // return; 此处不应该return 可以继续走后面的pop() ,否则return前需要加一次pop()
     }
 
-    dfs(node.left, cur);
-    dfs(node.right, cur);
+    if(node.left) dfs(node.left, cur);
+    if(node.right) dfs(node.right, cur);
 
     cur.pop();
   }
