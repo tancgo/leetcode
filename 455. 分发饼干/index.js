030410afb0b8295edf15d53cf3ff82ab71caea11
tancgo <542112893@qq.com>
2020-11-05 09:15:09 +0800

    分发饼干(贪心)

--- 455. 分发饼干/index.js
+++ 455. 分发饼干/index.js
@@ -9,3 +9,38 @@ var findContentChildren = function (g, s) {
   });
   return n;
 };
+
+// 第二次自己做  方法比别人的复杂了 = =
+var findContentChildren = function (g, s) {
+  let count = 0;
+  let start = 0; // 遍历起始位置
+
+  g.sort((a, b) => a - b);
+  s.sort((a, b) => a - b);
+
+  for (let i = 0; i < g.length; i++) {
+    for (let j = start; j < s.length; j++) {
+      if (g[i] <= s[j]) {
+        count++;
+        start = j + 1;
+        break;
+      }
+    }
+  }
+
+  return count;
+};
+
+// 优化 不用双层for循环了
+var findContentChildren = function (g, s) {
+  let n = 0;
+
+  g.sort((a, b) => a - b);
+  s.sort((a, b) => a - b);
+
+  for (const item of s) {
+    if (item >= g[n]) n++;
+  }
+
+  return n;
+};
