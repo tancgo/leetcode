8596d1464aa4d97198cf80beef35705c6ff94e4f
tancgo <542112893@qq.com>
2020-11-17 08:48:14 +0800

    链表有环(快慢指针)

--- 141. 环形链表/index.js
+++ 141. 环形链表/index.js
@@ -1,12 +1,28 @@
 var hasCycle = function (head) {
-  const map = new Map()
+  const map = new Map();
 
   while (head !== null) {
-    if (map.has(head.next)) return true
+    if (map.has(head.next)) return true;
 
-    map.set(head.next, 1)
-    head = head.next
+    map.set(head.next, 1);
+    head = head.next;
   }
 
-  return false
-};  
+  return false;
+};
+
+// 面试要求O1复杂度  采用快慢指针的解法  慢指针每次走一步 快指针每次走两步 如果有环  快慢指针终将会相遇
+var hasCycle = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast) {
+    slow = slow.next;
+    if (fast.next === null) return false;
+    fast = fast.next.next;
+
+    if (slow === fast) return true;
+  }
+
+  return false;
+};
